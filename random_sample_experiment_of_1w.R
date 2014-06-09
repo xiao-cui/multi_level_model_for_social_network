45339c8626b6a892e0fdce370405a787eff7a0b5
xiao-cui <cxstam@gmail.com>
2014-06-10 01:16:14 +1000

    move to 2nd abstraction level

--- random_sample_experiment_of_1w.R
+++ random_sample_experiment_of_1w.R
@@ -9,7 +9,6 @@ colnames(sample.graph)<-c("from","to")
 g<-graph.data.frame(sample.graph, vertices=sample.profile)
 
 #graph pre-processing
-
 #pre-processing 1
 #conver to undirected graph
 ug<-as.undirected(g, mode="mutual")
@@ -31,12 +30,22 @@ V(ug)$size<-degree(ug)
 par(mar=c(0,0,0,0))
 V(ug)$color=V(ug)$verified_type
 V(ug)$color=gsub("Unverified Account", "blue", V(ug)$color)
-V(ug)$color=gsub("DaRen|Application Software|Campus|Corporate Account|Government|Media|Organization|Personal Account|Website|Weibo Lady|8", "red", V(ug)$color)
+V(ug)$color=gsub("DaRen", "red", V(ug)$color)
+V(ug)$color=gsub("Application Software|Campus|Corporate Account|Government|Media|Organization|Personal Account|Website|Weibo Lady|8", "goldenrod1", V(ug)$color)
 plot(ug, vertex.label=NA, edge.arrow.mode=0)
 
 #retrieve articuation points
 cutpoint_ids=articulation.points(ug)
-V(ug)[cutpoint_ids]$color="darkorchid"
+V(ug)[cutpoint_ids]$color="deeppink1"
+plot(ug, vertex.label=NA, edge.arrow.mode=0)
+
+#retrieve articuation points whose degree greater than 10
+big.point.ids<-V(ug)[cutpoint_ids]$size>10
+#generate frequency table based on the types of verification
+t<-cbind(V(ug)[big.point.ids]$verified_type)
+freq.t<-table(t)
+par(mar=c(10,10,8,8))
+plot(freq.t, cex.axis=0.5, las=2, xlab="verified types", ylab="number of cut points")
 
 #construct 1 abstraction
 V(ug)$size=1
@@ -44,12 +53,47 @@ E(ug)$count=1
 comm.graph<-contract.vertices(ug, mc$membership, vertex.attr.comb=list(size="sum","ignore"))
 comm.graph<-simplify(comm.graph, remove.loops=TRUE, edge.attr.comb=list(count="sum","ignore"))
 V(comm.graph)$size<-V(comm.graph)$size/10
+par(mar=c(0,0,0,0))
 plot(comm.graph, vertex.label=NA, edge.arrow.mode=0)
 
 #retrieve articuation points
 cutpoint_ids=articulation.points(comm.graph)
-V(comm.graph)[cutpoint_ids]$color="darkorchid"
+V(comm.graph)[cutpoint_ids]$color="deeppink1"
 plot(comm.graph, vertex.label=NA, edge.arrow.mode=0)
+
+#retrieve articuation points
+V(comm.graph)[cutpoint_ids]
+#cut points locate within #5 and #35 communities
+V(ug)[mc$membership==5]
+V(ug)[mc$membership==35]
+#community 5
+#generate frequency table based on the types of verification
+t<-cbind(V(ug)[mc$membership==5]$verified_type)
+freq.t<-table(t)
+par(mar=c(10,10,8,8))
+plot(freq.t, cex.axis=0.5, las=2, xlab="verified types", ylab="number of cut points")
+#community 35
+#generate frequency table based on the types of verification
+t<-cbind(V(ug)[mc$membership==35]$verified_type)
+freq.t<-table(t)
+par(mar=c(10,10,8,8))
+plot(freq.t, cex.axis=0.5, las=2, xlab="verified types", ylab="number of cut points")
+
+#construct 2 abstraction
+mcmc<-multilevel.community(comm.graph)
+V(comm.graph)$size=1
+E(comm.graph)$count=1
+comm.comm.graph<-contract.vertices(comm.graph, mcmc$membership, vertex.attr.comb=list(size="sum","ignore"))
+comm.comm.graph<-simplify(comm.comm.graph, remove.loops=TRUE, edge.attr.comb=list(count="sum","ignore"))
+V(comm.comm.graph)$size<-V(comm.comm.graph)$size
+par(mar=c(0,0,0,0))
+plot(comm.comm.graph, vertex.label=NA, edge.arrow.mode=0)
+
+#retrieve articuation points
+cutpoint_ids=articulation.points(comm.comm.graph)
+V(comm.comm.graph)[cutpoint_ids]$color="deeppink1"
+plot(comm.comm.graph, vertex.label=NA, edge.arrow.mode=0)
+\
 
 
 
